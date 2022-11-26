948648677bc458b3ba38950ae05162cf2d3e885c
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2022-11-26 00:06:55 -0500

    update recipe card

--- app/src/main/java/com/example/tastyrecipes/ui/custom_views/RecipeCard.kt
+++ app/src/main/java/com/example/tastyrecipes/ui/custom_views/RecipeCard.kt
@@ -1,9 +1,8 @@
 package com.example.tastyrecipes.ui.custom_views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,11 +10,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -24,10 +25,11 @@ import com.example.tastyrecipes.R
 import com.example.tastyrecipes.domain.models.Recipe
 import com.example.tastyrecipes.domain.models.Topic
 import com.example.tastyrecipes.domain.models.UserRating
+import com.example.tastyrecipes.ui.theme.White
 
-val IMAGE_WIDTH = 310.dp
-val IMAGE_HEIGHT = 200.dp
-val CARD_HEIGHT = 420.dp
+val CARD_WIDTH = 300.dp
+val CARD_HEIGHT = 200.dp
+val IMAGE_HEIGHT = 500.dp
 
 @ExperimentalComposeUiApi
 @Composable
@@ -36,12 +38,11 @@ fun RecipeCard(
 ) {
     Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(CARD_HEIGHT)
+            .size(width = CARD_WIDTH, height = CARD_HEIGHT)
             .clickable { },
         elevation = 10.dp
     ) {
-        Column {
+        Box {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(recipe.thumbnail)
@@ -52,43 +53,29 @@ fun RecipeCard(
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .clip(RectangleShape)
-                    .size(width = IMAGE_WIDTH, height = IMAGE_HEIGHT)
+                    .size(width = CARD_WIDTH, height = IMAGE_HEIGHT)
             )
-            Column(
+            Box(
                 modifier = Modifier
-                    .padding(top = 12.dp, start = 12.dp, end = 12.dp)
+                    .align(Alignment.BottomCenter)
+                    .background(brush = Brush.verticalGradient(listOf(Color.Transparent, Color.Black)))
             ) {
-                Text(
-                    text = recipe.name,
-                )
-                TimeAndServingsView(
-                    prepTime = recipe.prepTimeMinutes,
-                    servings = recipe.servings,
-                )
-                StarRatingView(score = recipe.userRating.score)
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                var topicsString = recipe.topics.map { it.name }.joinToString(", ")
-                TextCard(topics = topicsString, IMAGE_WIDTH)
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(
-                    onClick = {},
-                    elevation = ButtonDefaults.elevation(
-                        defaultElevation = 10.dp,
-                        pressedElevation = 15.dp,
-                        disabledElevation = 0.dp
-                    ),
+                Column(
+                    modifier = Modifier
+                        .padding(top = 12.dp, start = 12.dp, end = 12.dp)
                 ) {
-                    Text(text = stringResource(id = R.string.lets_go))
+                    Text(
+                        text = recipe.name,
+                        color = White,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.width(CARD_WIDTH)
+                    )
+                    StarRatingView(
+                        score = recipe.userRating.score,
+                        textColor = Color.White,
+                        modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+                    )
                 }
             }
         }
--- app/src/main/java/com/example/tastyrecipes/ui/custom_views/StarRatingView.kt
+++ app/src/main/java/com/example/tastyrecipes/ui/custom_views/StarRatingView.kt
@@ -2,11 +2,15 @@ package com.example.tastyrecipes.ui.custom_views
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,20 +22,25 @@ import com.example.tastyrecipes.utils.getStarRating
 @ExperimentalComposeUiApi
 @Composable
 fun StarRatingView(
-    score: Double
+    score: Double,
+    modifier: Modifier = Modifier,
+    textColor: Color
 ) {
     val stars = getStarRating(score)
     Row(
+        modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Icon(painter = painterResource(
             id = R.drawable.ic_star_rate),
             contentDescription = "",
-            tint = StarYellow
+            tint = StarYellow,
+            modifier = Modifier.size(16.dp)
         )
         Text(
-            text = pluralStringResource(id = R.plurals.numberOfStarsRating, stars, stars)
+            text = pluralStringResource(id = R.plurals.numberOfStarsRating, stars, stars),
+            color = textColor
         )
     }
 }
@@ -41,5 +50,5 @@ fun StarRatingView(
 @Preview(showBackground = true)
 @Composable
 fun PreviewStarRatingView() {
-    StarRatingView(0.8881424253)
+    StarRatingView(0.8881424253, Modifier.padding(top = 8.dp), Color.White)
 }
